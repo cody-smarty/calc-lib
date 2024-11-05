cabf404e4ec039cba990c6b0a7906d118ef3893a
Cody Robertson <cody.robertson@smarty.com>
2024-11-05 10:34:21 -0700

    Added Subtraction, Multiplication, and Division

--- calc.go
+++ calc.go
@@ -1,7 +1,13 @@
 package calc
 
-type Addition struct{}
+type (
+	Addition       struct{}
+	Subtraction    struct{}
+	Multiplication struct{}
+	Division       struct{}
+)
 
-func (add *Addition) Calculate(a, b int) int {
-	return a + b
-}
+func (add *Addition) Calculate(a, b int) int       { return a + b }
+func (add *Subtraction) Calculate(a, b int) int    { return a - b }
+func (add *Multiplication) Calculate(a, b int) int { return a * b }
+func (add *Division) Calculate(a, b int) int       { return a / b } // No divide-by-zero protection
--- calc_test.go
+++ calc_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestCalculate(t *testing.T) {
+func TestCalculate_Addition(t *testing.T) {
 	testCases := []struct {
 		a, b int // Name is equation
 		want int
@@ -33,4 +33,99 @@ func TestCalculate(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestCalculate_Subtraction(t *testing.T) {
+	testCases := []struct {
+		a, b int // Name is equation
+		want int
+	}{
+		{a: 0, b: 0, want: 0},
+		{a: 2, b: 3, want: -1},
+		{a: -2, b: -3, want: 1},
+		// TODO -- overflow/underflow not handled
+	}
+
+	for _, tc := range testCases {
+		testName := fmt.Sprintf("%d + %d = %d", tc.a, tc.b, tc.want)
+		t.Run(testName, func(t *testing.T) {
+			// Setup
+			subtraction := &Subtraction{}
+
+			// Run test
+			result := subtraction.Calculate(tc.a, tc.b)
+
+			// Check results
+			if result != tc.want {
+				t.Errorf("want: %v, got: %v", tc.want, result)
+			}
+		})
+	}
+}
+
+func TestCalculate_Multiplication(t *testing.T) {
+	testCases := []struct {
+		a, b int // Name is equation
+		want int
+	}{
+		{a: 0, b: 0, want: 0},
+		{a: 2, b: 3, want: 6},
+		{a: -2, b: -3, want: 6},
+		// TODO -- overflow/underflow not handled
+	}
+
+	for _, tc := range testCases {
+		testName := fmt.Sprintf("%d + %d = %d", tc.a, tc.b, tc.want)
+		t.Run(testName, func(t *testing.T) {
+			// Setup
+			multiplication := &Multiplication{}
+
+			// Run test
+			result := multiplication.Calculate(tc.a, tc.b)
+
+			// Check results
+			if result != tc.want {
+				t.Errorf("want: %v, got: %v", tc.want, result)
+			}
+		})
+	}
+}
+
+func TestCalculate_Division(t *testing.T) {
+	testCases := []struct {
+		a, b int // Name is equation
+		want int
+	}{
+		{a: 0, b: 1, want: 0},
+		{a: 2, b: 3, want: 0},
+		{a: -2, b: -3, want: 0},
+		// TODO -- overflow/underflow not handled
+	}
+
+	for _, tc := range testCases {
+		testName := fmt.Sprintf("%d + %d = %d", tc.a, tc.b, tc.want)
+		t.Run(testName, func(t *testing.T) {
+			// Setup
+			division := &Division{}
+
+			// Run test
+			result := division.Calculate(tc.a, tc.b)
+
+			// Check results
+			if result != tc.want {
+				t.Errorf("want: %v, got: %v", tc.want, result)
+			}
+		})
+	}
+}
+
+func TestDivideByZeroPanics(t *testing.T) {
+	division := &Division{}
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Fatal("expected panic")
+		}
+	}()
+	division.Calculate(1, 0)
 }
